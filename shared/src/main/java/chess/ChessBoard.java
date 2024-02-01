989bda0e387f3c81690008fcb097bb4e944dc469
caleb <calebdmcdougal@gmail.com>
2024-01-31 17:19:30 -0700

    Generated toString methods

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -89,4 +89,11 @@ public class ChessBoard {
     public int hashCode() {
         return Arrays.deepHashCode(board);
     }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "board=" + Arrays.toString(board) +
+                '}';
+    }
 }

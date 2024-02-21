9e056052d1d9c15e7354163cf2df4d89dd7c655c
caleb <calebdmcdougal@gmail.com>
2024-02-21 14:10:27 -0700

    Added skeletons of the included files.  More needed.

---
+++ server/src/main/java/service/UserGameActions.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.AuthData;
+import model.UserData;
+
+public class UserGameActions {
+
+    public UserGameActions() {
+
+    }
+//    public AuthData register(UserData user) {
+//
+//    }
+//    public AuthData login(UserData user) {
+//
+//    }
+    public void clear() {
+
+    }
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,11 @@
+package service;
+
+public class UserService {
+
+    public UserService() {
+
+    }
+//    public AuthData register(UserData user) {}
+//    public AuthData login(UserData user) {}
+//    public void logout(UserData user) {}
+}

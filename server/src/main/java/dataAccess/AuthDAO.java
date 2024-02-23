1d974a0c249f09b4b32a66181a2f93fb9509b7db
caleb <calebdmcdougal@gmail.com>
2024-02-22 21:25:48 -0700

    Added methods to authDAO classes.

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -2,5 +2,8 @@ package dataAccess;
 
 public interface AuthDAO {
 
+    boolean validAuth(String authToken);
+    String createAuth();
     void clear();
+    int getDBSize();
 }
--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,19 +1,39 @@
 package dataAccess;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
 
 public class MemoryAuthDAO implements AuthDAO{
-    private static HashMap<Integer, String> AuthDB;
+    private static HashSet<String> AuthDB; // username, auth
 
     // Static block to initialize the HashMap for testing
     static {
-        AuthDB = new HashMap<>();
-        AuthDB.put(1, "One");
-        AuthDB.put(2, "Two");
-        AuthDB.put(3, "Three");
+        AuthDB = new HashSet<>();
+//        AuthDB.put(1, "One");
+//        AuthDB.put(2, "Two");
+//        AuthDB.put(3, "Three");
+    }
+
+    @Override
+    public boolean validAuth(String authToken) {
+        if (AuthDB.contains(authToken)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public String createAuth() {
+        String authToken = UUID.randomUUID().toString();
+        AuthDB.add(authToken);
+        return authToken;
     }
 
 
+    @Override
     public void clear() {
 //        System.out.println("in MemoryUserDAO clear");
 //        System.out.println(UserDB);

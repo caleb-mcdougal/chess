7c71be46364b367c616552e91448af55fef97b9e
caleb <calebdmcdougal@gmail.com>
2024-02-22 21:26:15 -0700

    Wrote and passed tests for AuthDao

---
+++ server/src/test/java/passoffTests/serverTests/AuthDAOTests.java
@@ -0,0 +1,32 @@
+package passoffTests.serverTests;
+
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+
+public class AuthDAOTests {
+
+    @Test
+    @DisplayName("Create unique authTokens")
+    public void createAuth(){
+        MemoryAuthDAO mad = new MemoryAuthDAO();
+        String auth1 = mad.createAuth();
+        System.out.println(auth1);
+        String auth2 = mad.createAuth();
+        System.out.println(auth2);
+        Assertions.assertNotEquals(auth1,auth2);
+    }
+
+    @Test
+    @DisplayName("Check authTokens")
+    public void checkAuth(){
+        MemoryAuthDAO mad = new MemoryAuthDAO();
+        String auth1 = mad.createAuth();
+        System.out.println(auth1);
+        Assertions.assertTrue(mad.validAuth(auth1));
+    }
+}

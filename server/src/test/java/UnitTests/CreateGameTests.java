dae785918144bc9b2793884d2de097f1129e15ef
caleb <calebdmcdougal@gmail.com>
2024-02-24 11:14:52 -0700

    Finished unit tests for create game

--- server/src/test/java/UnitTests/CreateGameTests.java
+++ server/src/test/java/UnitTests/CreateGameTests.java
@@ -44,4 +44,57 @@ public class CreateGameTests {
 
         Assertions.assertEquals(GameSizePre + 1,GameSizePost);
     }
+
+    @Test
+    @DisplayName("Create Game Negative Case")
+    public void CreateGameFailure(){
+        GameService gs = new GameService();
+        ChessGame game = new ChessGame();
+
+
+        UserData ud = new UserData("username", "password", "email");
+        MemoryAuthDAO mad = new MemoryAuthDAO();
+        String authToken = mad.createAuth(ud);
+
+        GameData gd = new GameData(0, "user1", "user2", "game1", game);
+        try{
+            gs.createGame(gd, "abc-123");
+        }
+        catch (Unauthorized e){
+            System.out.println("Unaothorized create game");
+            Assertions.assertTrue(true);
+        }
+        catch(BadRequestException e){
+            System.out.println("bad request create game");
+            Assertions.fail();
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("Create Game Negative Case Bad Request")
+    public void CreateGameFailureBadRequest(){
+        GameService gs = new GameService();
+        ChessGame game = new ChessGame();
+
+        UserData ud = new UserData("username", "password", "email");
+        MemoryAuthDAO mad = new MemoryAuthDAO();
+        String authToken = mad.createAuth(ud);
+
+        GameData gd = new GameData(0, "user1", "user2", "", game);
+        try{
+            gs.createGame(gd, authToken);
+        }
+        catch (Unauthorized e){
+            System.out.println("Unaothorized create game");
+            Assertions.fail();
+        }
+        catch(BadRequestException e){
+            System.out.println("bad request create game");
+            Assertions.assertTrue(true);
+        }
+
+
+    }
 }

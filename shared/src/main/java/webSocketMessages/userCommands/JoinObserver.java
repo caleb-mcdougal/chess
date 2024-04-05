79e467194a91661c98ee1506173312be300575e3
caleb <calebdmcdougal@gmail.com>
2024-04-05 13:10:32 -0600

    Added command types to UserGameCommand subclasses

--- shared/src/main/java/webSocketMessages/userCommands/JoinObserver.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinObserver.java
@@ -5,10 +5,12 @@ import chess.ChessGame;
 public class JoinObserver extends UserGameCommand{
 
     private int gameID;
+    private CommandType commandType;
 
     public JoinObserver(String authToken, int gameID) {
         super(authToken);
         this.gameID = gameID;
+        commandType = CommandType.JOIN_OBSERVER;
     }
 
     public int getGameID() {

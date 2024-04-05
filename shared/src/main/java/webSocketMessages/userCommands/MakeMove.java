85f224210cb1417c172a8a5d73d2221c302b4607
caleb <calebdmcdougal@gmail.com>
2024-04-05 12:40:49 -0600

    Added UserGameCommand subclasses

---
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMove.java
@@ -0,0 +1,24 @@
+package webSocketMessages.userCommands;
+
+import chess.ChessMove;
+import chess.ChessPiece;
+
+public class MakeMove extends UserGameCommand{
+
+    private int gameID;
+    private ChessMove move;
+
+    public MakeMove(String authToken, int gameID, ChessMove move) {
+        super(authToken);
+        this.gameID = gameID;
+        this.move = move;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+}
